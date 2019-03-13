70262973f5d1de74255cdcc543809a361b728025
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-13 19:15:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def board 
-  puts "["   |   |   ",
-]
+  puts ["   |   |   "]
+  puts ["   |   |   "]
+  puts ["   |   |   "]
+end
+
 def display_board(board)
   
 end
